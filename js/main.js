49416a79c168d8bf0d31ae50c4dc0414338e1aa0
royweb <xytank@gmail.com>
2019-11-21 10:56:41 -0500

    Add variables to get access to DOM elements

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 let time = 10;
 let score = 0;
 let isPlaying;
+
+// get DOM Elements
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
